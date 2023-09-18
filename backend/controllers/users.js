0898198fdbb82cc20738ef32b664455b79dd67c6
Naumkin Ilya <naumkinilya@yandex.ru>
2023-09-18 21:52:23 +0300

    Найти токен

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -114,7 +114,12 @@ const login = (req, res, next) => {
           }
           const payload = { _id: user._id };
           const token = jwt.sign(payload, NODE_ENV === 'production' ? SECRET_KEY : 'most-secret-key', { expiresIn: '7d' });
-          res.cookie('token', token, { httpOnly: true }).status(200).send({ message: 'Авторизация прошла успешно' });
+          res.cookie('token', token, {
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true,
+            domain: 'nomoredomainsicu.ru',
+          }).status(200).send({ message: 'Авторизация прошла успешно' });
         });
     })
     .catch((err) => {
